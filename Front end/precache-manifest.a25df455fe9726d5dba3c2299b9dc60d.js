809fb0de9e240c4ef6b52159f234f144c5b4aa35
chenkangyang <chenkangyang98@gmail.com>
2019-06-08 17:53:54 +0800

    update front_end

---
+++ Front end/precache-manifest.a25df455fe9726d5dba3c2299b9dc60d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f74cb26a30bbcae48480",
+    "url": "./static/css/main.9f4cdb12.chunk.css"
+  },
+  {
+    "revision": "f74cb26a30bbcae48480",
+    "url": "./static/js/main.f74cb26a.chunk.js"
+  },
+  {
+    "revision": "41301131c8e93bb59459",
+    "url": "./static/css/1.18a6fbd4.chunk.css"
+  },
+  {
+    "revision": "41301131c8e93bb59459",
+    "url": "./static/js/1.41301131.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "11dbca1aa55e341917ce0a0e871aadeb",
+    "url": "./static/media/404.11dbca1a.png"
+  },
+  {
+    "revision": "defb98583257610e959baa67ab0fa53b",
+    "url": "./static/media/beauty.defb9858.jpg"
+  },
+  {
+    "revision": "9f401ecedc40c975fd77bb1849175874",
+    "url": "./index.html"
+  }
+];
